814ee959d0d2665addab1eaaed4d23c1c87d0927
Flaviano O. Silva <fosbsb@gmail.com>
2015-08-16 14:52:56 +0000

    git-svn-id: svn://e2f.com.br/e2f/apps/scc_cerimonial@1070 f2c8e972-50ed-4da8-97be-2e774d6a7978

--- evento/novo-evento.php
+++ evento/novo-evento.php
@@ -1,26 +1,26 @@
 <?php 
-	include 'conexao/conn.php';
-	
-	$id 	= $_REQUEST['id'];
-	$acao   = $_REQUEST['acao'];
-	
-	if ($acao == "a"){
-		
-		$sql = "select * from evento where id = $id";		
-		$rs = mysqli_query($conexao, $sql);
-		
-		while ($linha = mysqli_fetch_array($rs)) {
-			$nome                  = $linha['nome'];
-			$data_inicio           = $linha['data_inicio'];
-			$data_fim              = $linha['data_fim'];
-			$descricao             = $linha['descricao'];
-			$local_id              = $linha['local_id'];
-			$usuario_cadastro_id   = $linha['usuario_cadastro_id'];
-		}
-	}else{
-		$acao = "n";
+include 'conexao/conn.php';
+
+$id 	= $_REQUEST['id'];
+$acao   = $_REQUEST['acao'];
+
+if ($acao == "a"){
+
+	$sql = "select * from evento where id = $id";
+	$rs = mysqli_query($conexao, $sql);
+
+	while ($linha = mysqli_fetch_array($rs)) {
+		$nome                  = $linha['nome'];
+		$data_inicio           = $linha['data_inicio'];
+		$data_fim              = $linha['data_fim'];
+		$descricao             = $linha['descricao'];
+		$local_id              = $linha['local_id'];
+		$usuario_cadastro_id   = $linha['usuario_cadastro_id'];
 	}
-	
+}else{
+	$acao = "n";
+}
+
 ?>
 <div class="row row-offcanvas row-offcanvas-right">
 	<div class="row">
@@ -42,61 +42,70 @@
 		</div>
 		<p>
 		
-		<form action="evento/gravar-evento.php?acao=<?php echo $acao?>" method="post">
+		
+		<form action="evento/gravar-evento.php?acao=<?php echo $acao?>"
+			method="post">
 			<input type="hidden" name="id" value="<?php echo $id?>">
 			<div class="col-lg-12">
-				<div class="input-group" style="margin:5px">
-					<span class="input-group-addon">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nome&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> 
-					<input type="text" name="nome" value="<?php echo $nome?>" class="form-control" placeholder="Digite o Nome do Evento" required>
+				<div class="input-group" style="margin: 5px">
+					<span class="input-group-addon">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nome&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+					<input type="text" name="nome" value="<?php echo $nome?>"
+						class="form-control" placeholder="Digite o Nome do Evento"
+						required>
 				</div>
 			</div>
-			
-			<span padding-top="5px"></span>				
-			
-			<div class="col-lg-4">
-				<div class="input-group" style="margin:5px">
-					<span class="input-group-addon">Data Evento</span> 
-					<input type="date" name="data_inicio" size="12" value="<?php echo $data_inicio?>" class="form-control" placeholder="Início" required>
-    		        <input type="date" name="data_fim" value="<?php echo $data_fim?>" class="form-control" placeholder="Fim" required>
-    		    </div>
-		    </div>
 
-		    <div class="row col-lg-12" style="padding-top: 5px;">
-				<span class="input-group-btn"> 
-					<div class="input-group">
-                       <span class="input-group-addon">&nbsp;&nbsp;&nbsp;&nbsp;Cidade&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                       <select name="cidade_id" class="form-control" style="width:400px" required>
-                          	<option value='' selected>-- Escolha uma cidade --</option>
-                              	<?php 
-                                  		
-                              		$sqlCidade = "select c.id, c.nome, u.sigla 
-                              		             from cidade c, uf u 
-                              		             where c.uf_id = u.id
-                              		             order by id";
-                              		$rsCidade = mysqli_query($conexao, $sqlCidade);
-                              		while($linha=mysqli_fetch_array($rsCidade)){
+			<span padding-top="1px"></span>
+
+			<div class="row" style="margin: 5px">
+				<div class="input-group col-xs-6 col-md-3">
+					<span class="input-group-addon">Data Evento</span> <input
+						type="date" name="data_inicio" value="<?php echo $data_inicio?>"
+						class="form-control" required> <input type="date" name="data_fim"
+						value="<?php echo $data_fim?>" class="form-control" required>
+				</div>
+			</div>
+
+			<div class="row" style="margin: 5px;">
+				<span class="input-group-btn">
+					<div class="input-group col-xs-8 col-md-4">
+						<span class="input-group-addon">&nbsp;&nbsp;&nbsp;&nbsp;Cidade&nbsp;&nbsp;&nbsp;&nbsp;</span>
+						<select name="cidade_id" class="form-control" style="width: 400px"
+							required>
+							<option value='' selected>-- Escolha uma cidade --</option>
+							<?php 
+
+							$sqlCidade = "select c.id, c.nome, u.sigla
+										from cidade c, uf u
+										where c.uf_id = u.id
+										order by id";
+							$rsCidade = mysqli_query($conexao, $sqlCidade);
+							while($linha=mysqli_fetch_array($rsCidade)){
                               			if ($linha['id'] == $cidade_id){
                               				$escolhido = "selected";
                               			}else{
                               				$escolhido = "";
                               			}
-										$opcao = "<option value='".$linha['id']."' ".$escolhido.">".$linha['nome']."-".$linha['sigla']."</option>";
+                              			$opcao = "<option value='".$linha['id']."' ".$escolhido.">".$linha['nome']."-".$linha['sigla']."</option>";
                               			echo $opcao;
-                              		}	
-                                  	
-                              	?>
-                        </select>
-                    </div>
+                              		}
+                              		 
+                              		?>
+						</select>
+				
 				</span>
 			</div>
-			
-			<div class="row col-lg-12" style="margin:5px">
-			    <span class="input-group-btn" style="padding-top: 10px">
-					<button class="btn btn-info" type="submit">Cadastrar</button>
-				</span>
-			</div>
-		</form>
-		</p>
-
+	
 	</div>
+
+	<div class="row" style="margin: 5px;">
+		<div class="row col-sm-6 col-md-4" style="margin: 5px">
+			<span class="input-group-btn" style="padding-top: 10px">
+				<button class="btn btn-info" type="submit">Cadastrar</button>
+			</span>
+		</div>
+	</div>
+
+	</form>
+	</p>
 </div>
