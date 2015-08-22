bec5d6c1a18bfa41e55d9c736c08acac1df64bea
Francisco Molina <francisco.m.duarte@gmail.com>
2015-08-22 13:49:45 +0000

    git-svn-id: svn://e2f.com.br/e2f/apps/scc_cerimonial@1108 f2c8e972-50ed-4da8-97be-2e774d6a7978

--- valida.php
+++ valida.php
@@ -1,6 +1,6 @@
 <?php 
 
-if (!$_SESSION["id_usuario"]){
+if (isset($_SESSION["id_usuario"]) && !$_SESSION["id_usuario"]){
 	echo "<script>location.href='login.php?erro=2';</script>";
 }else{
     //Verificar se o usuario tem permissao de acesso a pagina
